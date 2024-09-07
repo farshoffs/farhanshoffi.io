91f24d64c9f485aeccec87f452aa98a55d2d7116
farshoffs <farshoffs@gmail.com>
2024-09-07 13:49:46 +0800

    Delete ionic

--- health/rimbahealth/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.rimbahealth',
-  appName: 'rimbahealth',
-  webDir: 'www'
-};
-
-export default config;
