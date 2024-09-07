f7b7aaaffe1090d6d9333a8cb35b56569cc1539a
farshoffs <farshoffs@gmail.com>
2024-09-07 13:04:05 +0800

    Ionic Settings

--- health/rimbahealth/capacitor.config.ts
+++ health/rimbahealth/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.rimbahealth',
   appName: 'rimbahealth',
-  webDir: 'dist'
+  webDir: 'www'
 };
 
 export default config;
